b4dbf009c040d261d663c70566b777cc13f113a9
Matthew Foley <matthewfoley99@gmail.com>
2020-09-28 17:49:58 -0400

    Added loading animation still formatting homepage.

--- js/loading.js
+++ js/loading.js
@@ -2,3 +2,7 @@
 setTimeout(function () {
     document.querySelector(".loading").classList.add("hidden")
 }, 3000)
+var loadscreen = document.getElementById("homeload");
+setTimeout(function () {
+    loadscreen.parentElement.removeChild(loadscreen);
+}, 4500)
